dfef075709ce1d6087db5c58f4bb3ea69ac001be
KuanYu Chu <ckybonist@gmail.com>
2023-03-13 16:47:48 +0800

    feat: add window picker integration for neo-tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,6 +1,3 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
@@ -8,8 +5,35 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
+    "s1n7ax/nvim-window-picker",
   },
-  config = function ()
+  config = function()
+    -- Unless you are still migrating, remove the deprecated commands from v1.x
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+    local picker = require("window-picker")
+    picker.setup({
+      autoselect_one = true,
+      include_current = false,
+      filter_rules = {
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+          -- if the buffer type is one of following, the window will be ignored
+          buftype = { 'terminal', "quickfix" },
+        },
+      },
+      other_win_hl_color = '#e35e4f',
+    })
+    vim.keymap.set("n", "<leader>p", function()
+      local picked_window_id = picker.pick_window() or vim.api.nvim_get_current_win()
+      vim.api.nvim_set_current_win(picked_window_id)
+    end, { desc = "Pick a window" })
+
     require('neo-tree').setup {}
+
+    vim.cmd([[nnoremap \ :NeoTreeRevealToggle<cr>]])
   end,
 }
