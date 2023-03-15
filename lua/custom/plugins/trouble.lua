bc1533fdd7708b82e5a228b7a514f1c5931927db
KuanYu Chu <ckybonist@gmail.com>
2023-03-16 00:21:31 +0800

    feat: add plugins

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,10 @@
+-- A pretty diagnostics, references, telescope results, quickfix and location list
+return {
+  'folke/trouble.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function ()
+    require('trouble').setup({})
+  end
+}
