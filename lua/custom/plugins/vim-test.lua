edc440a6ab3e5c9f68179d987a794485612c8e55
KuanYu Chu <ckybonist@gmail.com>
2023-03-18 23:06:17 +0800

    feat: add neotest plugin

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -1,10 +1,10 @@
 return {
-  'vim-test/vim-test',
-  config = function()
-    vim.g['test#preserve_screen'] = 1
-    vim.g['test#neovim#start_normal'] = 1
-    vim.g['test#strategy'] = 'toggleterm'
-    vim.g['test#ruby#rspec#executable'] = 'rspec'
-    vim.g['test#neovim#term_position'] = 'bot 25'
-  end,
+  -- 'vim-test/vim-test',
+  -- config = function()
+  --   vim.g['test#preserve_screen'] = 1
+  --   vim.g['test#neovim#start_normal'] = 1
+  --   vim.g['test#strategy'] = 'toggleterm'
+  --   vim.g['test#ruby#rspec#executable'] = 'rspec'
+  --   vim.g['test#neovim#term_position'] = 'bot 25'
+  -- end,
 }
