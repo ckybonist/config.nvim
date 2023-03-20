f0c301f410f24154347c9e23edbe4a40a9fba590
KuanYu Chu <ckybonist@gmail.com>
2023-03-21 00:08:49 +0800

    chore: use KeymapOpts

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,27 +3,29 @@ return {
   config = function()
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
-    local opts = { noremap = true, silent = true }
 
     vim.keymap.set('n', '<leader>ha', function()
       mark.add_file()
-    end, vim.tbl_extend('keep', opts, { desc = '[H]arpoon [A]dd file' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = '[H]arpoon [A]dd file' })))
 
     vim.keymap.set('n', '<C-e>', function()
       ui.toggle_quick_menu()
-    end, vim.tbl_extend('keep', opts, { desc = 'Harpoon: toggle quick menu' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = 'Harpoon: toggle quick menu' })))
 
     vim.keymap.set('n', '<C-h>', function()
       ui.nav_file(1)
-    end, vim.tbl_extend('keep', opts, { desc = 'Harpoon: navigate to entry 1' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = 'Harpoon: navigate to entry 1' })))
+
     vim.keymap.set('n', '<C-t>', function()
       ui.nav_file(2)
-    end, vim.tbl_extend('keep', opts, { desc = 'Harpoon: navigate to entry 2' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = 'Harpoon: navigate to entry 2' })))
+
     vim.keymap.set('n', '<C-n>', function()
       ui.nav_file(3)
-    end, vim.tbl_extend('keep', opts, { desc = 'Harpoon: navigate to entry 3' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = 'Harpoon: navigate to entry 3' })))
+
     vim.keymap.set('n', '<C-s>', function()
       ui.nav_file(4)
-    end, vim.tbl_extend('keep', opts, { desc = 'Harpoon: navigate to entry 4' }))
+    end, vim.tbl_extend('keep', KeymapOpts({ desc = 'Harpoon: navigate to entry 4' })))
   end,
 }
