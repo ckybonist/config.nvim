52acce21ea6f55a493741c2335e7a05f8c90616f
KuanYu Chu <ckybonist@gmail.com>
2023-03-23 11:21:34 +0800

    feat: add keymaps for Rails project commands

--- after/plugin/commands.lua
+++ after/plugin/commands.lua
@@ -7,33 +7,22 @@ create_command('Gbackend', function()
     type_filter = 'backend',
   })
 end, {})
+vim.keymap.set('n', '<leader>fb', vim.cmd.Gbackend, KeymapOpts({ desc = '[F]ind [B]ackend code'}))
+
+create_command('Grspec', function()
+  telescope_builtin['live_grep']({
+    search_dirs = { 'spec' },
+    type_filter = 'backend',
+  })
+end, {})
+vim.keymap.set('n', '<leader>fsp', vim.cmd.Grspec, KeymapOpts({ desc = '[F]ind [S][P]ec'}))
 
 create_command('Gjs', function()
   telescope_builtin['live_grep']({
     type_filter = 'ecmascript',
   })
 end, {})
-
-create_command('GRSpec', function()
-  telescope_builtin['live_grep']({
-    search_dirs = { 'spec' },
-    type_filter = 'backend',
-  })
-end, {})
-
-create_command('ReloadConfig', function()
-  local hls_status = vim.v.hlsearch
-  for name, _ in pairs(package.loaded) do
-    if name:match('^accky') then
-      package.loaded[name] = nil
-    end
-  end
-
-  dofile(vim.env.MYVIMRC)
-  if hls_status == 0 then
-    vim.opt.hlsearch = false
-  end
-end, {})
+vim.keymap.set('n', '<leader>fjs', vim.cmd.Gjs, KeymapOpts({ desc = '[F]ind [J][S] code'}))
 
 create_command('LspDefinition', function()
   vim.lsp.buf.definition()
