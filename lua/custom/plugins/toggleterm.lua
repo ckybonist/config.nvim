30e241bbd272b2bbde4e878639f045910c56f68f
KuanYu Chu <ckybonist@gmail.com>
2023-03-28 18:01:44 +0800

    fix: keymap

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require('toggleterm').setup({
-      open_mapping = [[\]],
+      open_mapping = [[`t]],
       direction = 'float',
       float_opts = { direction = 'float', border = 'curved' },
     })
