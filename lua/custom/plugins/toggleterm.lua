b5affe245072666909567666b31b31789cc4eda3
KuanYu Chu <ckybonist@gmail.com>
2023-03-29 15:27:33 +0800

    enhance: adjust toggleterm keymap

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require('toggleterm').setup({
-      open_mapping = [[`t]],
+      open_mapping = [[<c-`>]],
       direction = 'float',
       float_opts = { direction = 'float', border = 'curved' },
     })
