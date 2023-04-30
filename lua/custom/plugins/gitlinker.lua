a1c24a02aed2708df52c3f642e32a2980f4c3bcd
KuanYu Chu <ckybonist@gmail.com>
2023-05-01 00:47:09 +0800

    chore(gitlinker): add keymap description

--- lua/custom/plugins/gitlinker.lua
+++ lua/custom/plugins/gitlinker.lua
@@ -10,10 +10,10 @@ return {
 
     vim.keymap.set('n', '<leader>gl', function()
       gitlinker.get_buf_range_url('n', { action_callback = require('gitlinker.actions').open_in_browser })
-    end, { silent = true })
+    end, KeymapOpts({ silent = true, desc = 'Browse GitHub permalnk' }))
 
     vim.keymap.set('v', '<leader>gl', function()
       gitlinker.get_buf_range_url('v', { action_callback = require('gitlinker.actions').open_in_browser })
-    end, {})
+    end, KeymapOpts({ silent = true, desc = 'Browse GitHub permalnk' }))
   end,
 }
