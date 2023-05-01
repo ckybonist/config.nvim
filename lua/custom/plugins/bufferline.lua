1c6d0fa88844a3060e214eb11f7ca69d1d27feec
KuanYu Chu <ckybonist@gmail.com>
2023-05-02 00:17:09 +0800

    feat: adjust bufferline styles

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,7 +3,30 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup({})
+    require('bufferline').setup({
+      options = {
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { 'close' },
+        },
+        buffer_close_icon = '',
+        close_icon = '',
+        indicator = {
+          icon = '▎', -- this should be omitted if indicator style is not 'icon'
+          style = 'icon',
+        },
+        separator_style = 'slope',
+        show_tab_indicators = true,
+      },
+      highlights = {
+        buffer_selected = {
+          fg = '#cb4b16',
+          sp = '#cb4b16',
+          bold = true,
+        },
+      },
+    })
 
     vim.keymap.set('n', '<leader>bp', ':BufferLinePick<CR>', KeymapOpts({ desc = '[B]uffer [P]ick' }))
   end,
