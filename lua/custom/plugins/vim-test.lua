c92c501eac27df968065da2c724ed8393580ca7b
KuanYu Chu <ckybonist@gmail.com>
2023-05-08 14:56:55 +0800

    fix: rspec bin path

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -4,7 +4,6 @@ return {
     vim.g['test#preserve_screen'] = 1
     vim.g['test#neovim#start_normal'] = 1
     vim.g['test#strategy'] = 'toggleterm'
-    vim.g['test#ruby#rspec#executable'] = 'rspec'
     vim.g['test#neovim#term_position'] = 'bot 25'
 
     vim.keymap.set('n', '<leader>tf', vim.cmd.TestFile, KeymapOpts({ desc = '[T]est [F]ile' }))
