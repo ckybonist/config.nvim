99fc4930b25361e28240de401415629edca7f4d7
KuanYu Chu <ckybonist@gmail.com>
2023-05-20 15:43:02 +0800

    use kanagawa-dragon

--- lua/custom/plugins/kanagawa.lua
+++ lua/custom/plugins/kanagawa.lua
@@ -12,7 +12,7 @@ return {
       transparent = true, -- do not set background color
       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
       terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      theme = 'wave', -- Load "wave" theme when 'background' option is not set
+      theme = 'dragon', -- Load theme (lotus/wave/dragon) when 'background' option is not set
       background = {
         -- map the value of 'background' option to a theme
         dark = 'dragon',
