345153ad59ee20718f961647aca9af3364fc72ce
KuanYu Chu <ckybonist@gmail.com>
2023-05-23 21:04:22 +0800

    fix(copilot): keymap conflicts

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -2,5 +2,10 @@ return {
   'github/copilot.vim',
   config = function()
     vim.g.copilot_ignore_node_version = true
+    -- Prevent keymap conflicts
+    vim.g.copilot_no_tab_map = true
+    vim.cmd([[
+      imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+    ]])
   end,
 }
