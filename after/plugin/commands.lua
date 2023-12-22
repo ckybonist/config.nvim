86c8090d2f46b91f55ebaf6df7be4de2bfa197df
KuanYu Chu <ckybonist@gmail.com>
2023-12-22 22:19:10 +0800

    enhance: rails project greping config

--- after/plugin/commands.lua
+++ after/plugin/commands.lua
@@ -1,28 +1,32 @@
 local create_command = vim.api.nvim_create_user_command
-local telescope_builtin = require('telescope.builtin')
+local ts = require('telescope.builtin')
+local base_opts = { layout_config = { width = 140 } }
 
 -- type_filter's type defined in ripgreprc
 create_command('Gbackend', function()
-  telescope_builtin['live_grep']({
-    search_dirs = { 'app', 'config', 'lib', 'modules', 'spec' },
-    type_filter = 'backendRoR',
-  })
+  local opts = vim.tbl_extend(
+    'keep',
+    base_opts,
+    { search_dirs = { 'app', 'config', 'lib', 'modules', 'spec' }, type_filter = 'backendRoR' }
+  )
+  ts.live_grep(require('telescope.themes').get_dropdown(opts))
 end, {})
 vim.keymap.set('n', '<leader>fb', vim.cmd.Gbackend, KeymapOpts({ desc = '[F]ind [B]ackend code' }))
 
 create_command('Grspec', function()
-  telescope_builtin['live_grep']({
-    search_dirs = { 'spec' },
-    type_filter = 'backendRoR',
-  })
+  local opts = vim.tbl_extend('keep', base_opts, { search_dirs = { 'spec' }, type_filter = 'backendRoR' })
+  ts.live_grep(require('telescope.themes').get_dropdown(opts))
 end, {})
 
 create_command('Gjs', function()
-  telescope_builtin['live_grep']({
-    type_filter = 'ecmascript',
-  })
+  local opts = vim.tbl_extend('keep', base_opts, { type_filter = 'js' })
+  ts.live_grep(require('telescope.themes').get_dropdown(opts))
 end, {})
-vim.keymap.set('n', '<leader>fjs', vim.cmd.Gjs, KeymapOpts({ desc = '[F]ind [J][S] code' }))
+
+create_command('Gstyle', function()
+  local opts = vim.tbl_extend('keep', base_opts, { type_filter = 'stylesheet' })
+  ts.live_grep(require('telescope.themes').get_dropdown(opts))
+end, {})
 
 create_command('NLFormat', function()
   vim.lsp.buf.format({ async = true })
