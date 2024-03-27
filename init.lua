32097ac0d376e6b34133238049cd35aaf2b3fc31
KuanYu Chu <ckybonist@gmail.com>
2024-03-27 20:57:14 +0800

    fix: Copilot config

--- init.lua
+++ init.lua
@@ -469,10 +469,9 @@ require('lazy').setup({
           end, { 'i', 's' }),
         }),
         sources = {
-          { name = 'copilot', group_index = 2 },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
+          { name = 'nvim_lsp', group_index = 2 },
+          { name = 'path', group_index = 2 },
+          { name = 'luasnip', group_index = 2 },
         },
       })
     end,
